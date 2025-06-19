86f16138dad446a7630392725d994d3a30b5a7c3
Armpa16 <panudech1419@gmail.com>
2025-06-19 15:48:47 +0700

    Update LoginPage

--- loginform.php
+++ loginform.php
@@ -60,5 +60,23 @@
         </div>
     </div>
     <!-- box_right -->
+     <script>
+        const togglePassword = document.querySelector('#togglePassword');
+        const password = document.querySelector('#password');
+
+        togglePassword.addEventListener('click', function (e) {
+            // toggle the type attribute
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            // toggle the eye slash icon
+            if (type === 'password') {
+                this.classList.remove('fa-eye-slash');
+                this.classList.add('fa-eye');
+            } else {
+                this.classList.remove('fa-eye');
+                this.classList.add('fa-eye-slash');
+            }
+        });
+    </script>
 </body>
 </html>
